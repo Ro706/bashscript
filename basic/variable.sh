43c2fa267c6c31efd63fd9943fedc637723df840
Ro706 <rohitmandal99091@gmail.com>
2026-05-07 23:06:07 +0530

    Add [07-05-2026] Add variable declaration method

---
+++ basic/variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Declaring string and integer variable 
+# note:
+# when ever you declare a variable in bash then variable must be declare in this way ->
+# name="name"
+# number=100
+# Remember never leave blank space between variable and equal to
+
+number=12
+name="Rohit"
+echo "Name is $name"
+echo "class is $number"
+
+# Inbuild Environment Variables
+
+# Display the PATH environment variable
+echo "Your PATH is $PATH"
